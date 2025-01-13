60acba471d599eeba9c5c415af3f2fcaf39efab0
Chris-2801 <christopherv123sb@gmail.com>
2025-01-12 22:15:18 -0500

    Primer commit

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -o errexit
 
-py - m pip install -r requirements.txt -or python -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
-py manage.py collectstatic --noinput -or python manage.py collectstatic
-py manage.py migrate -or python manage.py migrate
+python manage.py collectstatic
+python manage.py migrate
